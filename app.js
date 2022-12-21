9a757a284ad287a31b7a9d00dc46c9852691f03a
03Lanka <jhp1403@naver.com>
2022-12-21 21:35:53 +0900

    arc, beginPath, fillStyle의 사용법에 대해 배웠습니다. (#1.5)

--- app.js
+++ app.js
@@ -18,12 +18,27 @@ canvas.height = 800;
 // ctx.fill(); // 그려진 사각형의 내부를 채웁니다.
 // ~#1.3
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2; // lineWidth는 선의 두께를 지정합니다. (이 코드 아래부터 그려지는 선에 적용됩니다.)
-ctx.strokeRect(300, 300, 50, 100); // strokeRect는 중앙이 비어있는 사각형을 그립니다.
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
-ctx.fill(); // stroke, fill은 lineTo 아래에 있어야합니다.
+// ctx.fillRect(200, 200, 50, 200);
+// ctx.fillRect(400, 200, 50, 200);
+// ctx.lineWidth = 2; // lineWidth는 선의 두께를 지정합니다. (이 코드 아래부터 그려지는 선에 적용됩니다.)
+// ctx.strokeRect(300, 300, 50, 100); // strokeRect는 중앙이 비어있는 사각형을 그립니다.
+// ctx.fillRect(200, 200, 200, 20);
+// ctx.moveTo(200, 200);
+// ctx.lineTo(325, 100);
+// ctx.lineTo(450, 200);
+// ctx.fill(); // stroke, fill은 lineTo 아래에 있어야합니다. (투명으로 그려진 선에 색을 넣어주는 방식입니다.)
+// ~#1.4
+
+ctx.fillRect(170, 200 - 10, 15, 100);
+ctx.fillRect(310, 200 - 10, 15, 100);
+ctx.fillRect(220, 200 - 10, 60, 200);
+ctx.stroke();
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI); // arc는 원을 그리는 도구로써, (X, Y, 반지름, 시작 각, 끝 각)이 입력됩니다. 원을 그릴때는, 시작 각을 0으로 두고 끝 각을 상수 * Math.PI로 구성하면 됩니다.
+ctx.fill();
+
+ctx.beginPath(); // 특정 그림에 색을 입히고자 한다면, beginPath를 이용하여 해당 그림을 독립적으로 만들어줘야합니다.
+ctx.fillStyle = "white"; // fillStyle은 그림의 색을 지정할 수 있습니다.
+ctx.arc(230, 90, 8, Math.PI, 2 * Math.PI);
+ctx.arc(270, 90, 8, Math.PI, 2 * Math.PI);
+ctx.fill();
